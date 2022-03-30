7d12783f2ce18b43f2c64e810da5d401d6b9c979
CsYBot <67687653+CsYBot@users.noreply.github.com>
2022-03-30 22:24:35 +0300

    Update encrypt.js

--- src/encrypt.js
+++ src/encrypt.js
@@ -3,17 +3,33 @@ const encryptor = require('cryptr');
 module.exports = function(SECRET_KEY) {
   const cipher = new encryptor(SECRET_KEY);
   
+  this.json = function(str) {
+    try {
+      JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+  }
+
   this.encrypt = function(value) {
+    if (typeof value === "object") value = JSON.stringify(value);
+
     let ciphertext = cipher.encrypt(value).toString();
     return ciphertext;
   }
   
   this.decrypt = function(value) {
     try {
-      let ciphertext = cipher.decrypt(value).toString();
-      return (ciphertext) ? ciphertext : null;
+      let ciphertext = cipher.decrypt(value);
+      let control = this.json(ciphertext);
+      if(control == false) {
+        return (ciphertext) ? ciphertext : null;
+      } else {
+        return (ciphertext) ? JSON.parse(ciphertext) : null;
+      }
     } catch (err) {
-      return null
+      return null;
     }
   }
 }
